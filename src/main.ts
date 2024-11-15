1d14c16bd206c160526e58eb885d55edf00ca5ba
yhan <yannick.han@easybourse.com>
2024-11-15 11:53:25 +0100

    Init Project

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+
+import { routes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
+
+import { initializeApp } from "firebase/app";
+import { importProvidersFrom } from "@angular/core";
+import { IonicModule } from "@ionic/angular";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxdKn0omeczTca37dRGzEiKGcB-3UXXKc",
+  authDomain: "car-rental-agency-exam.firebaseapp.com",
+  projectId: "car-rental-agency-exam",
+  storageBucket: "car-rental-agency-exam.firebasestorage.app",
+  messagingSenderId: "719803502125",
+  appId: "1:719803502125:web:535b8e04a57c7525df72a4"
+};
+
+initializeApp(firebaseConfig);
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    {
+      provide: RouteReuseStrategy, useClass: IonicRouteStrategy
+    },
+      importProvidersFrom(IonicModule.forRoot({innerHTMLTemplatesEnabled: true})),
+      provideIonicAngular(),
+      provideRouter(routes, withPreloading(PreloadAllModules)),
+  ],
+});
